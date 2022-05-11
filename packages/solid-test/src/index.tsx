14f1b7be21eab35247d25f3fb2dbb48387855fa2
yellowsink <yellowsink@riseup.net>
2022-05-11 09:58:02 +0000

    idfk

--- packages/solid-test/src/index.tsx
+++ packages/solid-test/src/index.tsx
@@ -7,17 +7,62 @@ import crossani from "solid-crossani";
 // minified away, stops TSC removing the import
 false && crossani;
 
-import { EASE } from "crossani";
+import { EASE, PartialTransition } from "crossani";
+
+const transitions = {
+  default: {
+    reset: true,
+    ms: 500,
+  },
+
+  step1: {
+    state: {
+      "background-color": "blue",
+      border: "2px dashed red",
+    },
+    ms: 250,
+    easing: EASE.linear,
+  },
+
+  step2: {
+    state: {
+      width: "250px",
+      height: "90px",
+    },
+    ms: 300,
+    easing: EASE.in,
+  },
+
+  step3: {
+    state: {
+      transform: "rotate(170deg)",
+      "margin-left": "15rem",
+    },
+    ms: 600,
+    easing: EASE.out,
+  },
+};
+
+const step4 = {
+  state: {
+    background: "radial-gradient(circle at 110px, red, green)",
+    "border-radius": "50%",
+    "box-shadow": "0 0 10px black",
+    transform: "rotate(170deg) translateX(50px) skewY(40deg)",
+  },
+  ms: 150,
+  easing: EASE.inOut,
+};
 
 const App: Component = () => {
-  const [trigger, setTrigger] = createSignal("");
-  const [current, setCurrent] = createSignal<string>();
+  const [trigger, setTrigger] = createSignal<string | PartialTransition>("");
+  const [current, setCurrent] = createSignal<string | PartialTransition>();
 
   function triggerAnimation() {
     setTrigger("step1");
     setTrigger("step2");
     setTrigger("step3");
-    setTrigger("step4");
+    setTrigger(step4);
   }
 
   return (
@@ -26,7 +71,13 @@ const App: Component = () => {
         <button onClick={triggerAnimation}>trigger</button>
         <button onClick={() => setTrigger("default")}>reset</button>
 
-        {current() ?? <em>Not currently animating</em>}
+        {typeof current() === "string" ? (
+          current()
+        ) : current === undefined ? (
+          <em>Not currently animating</em>
+        ) : (
+          <em>Custom animation</em>
+        )}
       </div>
 
       <div
@@ -41,52 +92,7 @@ const App: Component = () => {
           "margin-top": "1rem",
         }}
         // @ts-expect-error - currently figuring out a way to do this
-        prop:transitions={{
-          default: {
-            state: {},
-            reset: true,
-            ms: 500,
-            easing: EASE.ease,
-          },
-
-          step1: {
-            state: {
-              "background-color": "blue",
-              border: "2px dashed red",
-            },
-            ms: 250,
-            easing: EASE.linear,
-          },
-
-          step2: {
-            state: {
-              width: "250px",
-              height: "90px",
-            },
-            ms: 300,
-            easing: EASE.in,
-          },
-
-          step3: {
-            state: {
-              transform: "rotate(170deg)",
-              "margin-left": "15rem",
-            },
-            ms: 600,
-            easing: EASE.out,
-          },
-
-          step4: {
-            state: {
-              background: "radial-gradient(circle at 110px, red, green)",
-              "border-radius": "50%",
-              "box-shadow": "0 0 10px black",
-              transform: "rotate(170deg) translateX(50px) skewY(40deg)",
-            },
-            ms: 150,
-            easing: EASE.inOut,
-          },
-        }}
+        prop:transitions={transitions}
       />
     </>
   );
